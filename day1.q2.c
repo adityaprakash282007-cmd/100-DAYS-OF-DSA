697ff247cc8563dff938f26dff09510c2e97a5d9
adityaprakash282007-cmd <adityaprakash282007@gmail.com>
2026-02-01 19:07:32 +0530

    Add files via upload

---
+++ day1.q2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int n, target;
+
+    // Input size of array
+    scanf("%d", &n);
+
+    int nums[n];
+
+    // Input array elements
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &nums[i]);
+    }
+
+    // Input target
+    scanf("%d", &target);
+
+    // Find two sum
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (nums[i] + nums[j] == target) {
+                printf("[%d, %d]\n", i, j);
+                return 0;
+            }
+        }
+    }
+
+    return 0;
+}
